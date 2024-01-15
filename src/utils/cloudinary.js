0daec85821350392589511a2a17f7f1d3f6f22be
Harshit Tyagi <harshitt152@gmail.com>
2024-01-15 21:56:02 +0530

    Added util fun to upload file

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFile = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      format: "auto",
+    });
+    //File has been uploaded successfully
+    console.log("File has been uploaded");
+    //to remove file from server  after  upload
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    //to remove file from server if not uploaded
+    fs.unlinkSync(localFilePath);
+    console.log();
+    return null;
+  }
+};
+export default uploadFile;
