343ee8aee9672b97149e9f7390be88d6121de514
Harshit Tyagi <harshitt152@gmail.com>
2024-01-16 22:18:32 +0530

    Added ToDo's in register COntroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,13 @@
 export const registerUser = async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
   res.status(200).json({
     message: "User Registered",
   });
