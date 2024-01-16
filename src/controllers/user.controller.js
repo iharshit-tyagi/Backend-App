4e83ee425ae024844b9704127bfbcc03919b7b3f
Harshit Tyagi <harshitt152@gmail.com>
2024-01-16 22:36:53 +0530

    Added Data validation on user data in register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 export const registerUser = async (req, res) => {
-  // get user details from frontend
-  // validation - not empty
+  // get user details from frontend- done
+  // validation - not empty -done
   // check if user already exists: username, email
   // check for images, check for avatar
   // upload them to cloudinary, avatar
@@ -8,9 +8,16 @@ export const registerUser = async (req, res) => {
   // remove password and refresh token field from response
   // check for user creation
   // return res
-  res.status(200).json({
-    message: "User Registered",
-  });
+  const userData = req.body;
+  const { userName, email, fullName } = userData;
+  if (!userName || !email || !fullName) {
+    res.status(401).json({
+      message: "Including USername , Email &  Full name is mandatory",
+    });
+  } else
+    res.status(200).json({
+      message: userName,
+    });
 };
 
 export const loginUser = async (req, res) => {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,5 +5,8 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 
 userRouter.post("/login", loginUser);
+userRouter.post("/login", (req, res) => {
+  req.body;
+});
 
 export default userRouter;
