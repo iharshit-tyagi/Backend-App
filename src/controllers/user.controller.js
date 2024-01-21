82d88296750229239a8c7b2656fa245ed9f6bf0b
Harshit Tyagi <harshitt152@gmail.com>
2024-01-21 18:04:32 +0530

    Added Validation to check is user already Exists

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,23 +1,38 @@
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+
 export const registerUser = async (req, res) => {
-  // get user details from frontend- done
-  // validation - not empty -done
-  // check if user already exists: username, email
+  -done;
+
   // check for images, check for avatar
   // upload them to cloudinary, avatar
   // create user object - create entry in db
   // remove password and refresh token field from response
   // check for user creation
   // return res
+
+  // get user details from frontend
   const userData = req.body;
-  const { userName, email, fullName } = userData;
-  if (!userName || !email || !fullName) {
-    res.status(401).json({
-      message: "Including USername , Email &  Full name is mandatory",
-    });
-  } else
-    res.status(200).json({
-      message: userName,
-    });
+  const { userName, email, fullName, password } = userData;
+
+  // validation - not empty -done
+  if (!userName || !email || !fullName || !password) {
+    res.send(new ApiError(401, "One or More  required Parameter is Empty"));
+  }
+
+  // check if user already exists: username, email
+  const userExist = await User.findOne({
+    //to search for one document with or condition
+    $or: [
+      {
+        username: userName,
+        email: email,
+      },
+    ],
+  });
+  if (userExist) {
+    res.send(new ApiError(410, "This Username or Email Already Exists"));
+  }
 };
 
 export const loginUser = async (req, res) => {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const userRouter = express.Router();
 
-userRouter.post("/register", registerUser);
+userRouter.post(
+  "/register",
 
-userRouter.post("/login", loginUser);
+  (req, res) => {
+    registerUser(req, res);
+  }
+);
+
+userRouter.post("/login", (req, res, next) => {
+  loginUser(req, res);
+});
 userRouter.post("/login", (req, res) => {
   req.body;
 });
